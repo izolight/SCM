090bfb5b0e646e2c2f84acbb593ec1ed17f41231
Alain Keusen <alain@diemeute.ch>
2017-10-06 10:35:16 +0200

    feature/base_structure - add page 'add member'

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index),
+    url(r'^$', views.index),
+    url(r'^add_member/', views.add_member),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def add_member(request):
+    return render(request, 'add_member.html')
