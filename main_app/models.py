5c6a3ec00ea4d92f2b2717ab60469a8d71564668
Gabor Tanz <gabor.tanz@gmail.com>
2017-10-11 20:32:20 +0200

    add member model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.ForeignKey(City, related_name='members')
+    phone_regex = RegexValidator(regex=r'^\+41\d{9}$', message="Bitte gültige Telefonnummer eingeben")
+    phone_number = models.CharField(validators=[phone_regex], max_length=12)
+    email_regex = RegexValidator(regex=r'^[^@\s]+@[^@\s]+\.[^@\s]+$', message="Bitte gültige email angeben") # i know this is stupid
+    email = models.CharField(validators=[email_regex], max_length=100)
+    user_name = models.CharField(max_length=50) # TODO connect with builtin auth
+
+
+class City (models.Model):
+    zip_regex = RegexValidator(regex=r'^\d{4}$', message="Bitte vierstellige PLZ eingeben")
+    zip_code = models.PositiveSmallIntegerField(validators=[zip_regex], unique=True)
+    city = models.CharField(max_length=30)
