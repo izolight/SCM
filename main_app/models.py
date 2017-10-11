92a87c73e87121ff44d861506126f40241676aff
Gabor Tanz <gabor.tanz@gmail.com>
2017-10-11 20:51:49 +0200

    use builtin user model and add member info with 1-to-1 relationship

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Member(models.Model):
@@ -10,8 +13,20 @@ class Member(models.Model):
     phone_regex = RegexValidator(regex=r'^\+41\d{9}$', message="Bitte gültige Telefonnummer eingeben")
     phone_number = models.CharField(validators=[phone_regex], max_length=12)
     email = models.EmailField(max_length=200)
-    user_name = models.CharField(max_length=50, unique=True)  # TODO connect with builtin auth
+    user_name = models.OneToOneField(User, on_delete=models.CASCADE)
     website = models.URLField(max_length=100, blank=True)
+
+
+# see https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+@receiver(post_save, sender=User)
+def create_member(sender, instance, created, **kwargs):
+    if created:
+        Member.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_member(sender, instance, **kwargs):
+    instance.member.save()
 
 
 class City (models.Model):
