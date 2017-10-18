6d099134cd3997f8fda9495723d259173efb0acf
Gabor Tanz <gabor.tanz@gmail.com>
2017-10-18 17:33:45 +0200

    remove fields that were already in builtin auth_user table

--- main_app/models.py
+++ main_app/models.py
@@ -12,13 +12,10 @@ class City(models.Model):
 
 
 class Member(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.ForeignKey(City, related_name='members')
     phone_regex = RegexValidator(regex=r'^\+41\d{9}$', message="Bitte gültige Telefonnummer eingeben")
     phone_number = models.CharField(validators=[phone_regex], max_length=12)
-    email = models.EmailField(max_length=200)
     user_name = models.OneToOneField(User, on_delete=models.CASCADE)
     website = models.URLField(max_length=100, blank=True)
 
