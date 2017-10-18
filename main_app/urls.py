9a0fa9a56d22cb9e269d380ee1cee60b9a094606
Marcus Fischer <fiscm7@bfh.ch>
2017-10-18 17:22:27 +0200

    features/navigation - added the navigation - added the files which are linked on the navigation

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,16 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^add_member/', views.add_member),
-    url(r'^directory/', views.directory),
+    url(r'^member_add/', views.member_add),
+    url(r'^member_directory/', views.member_directory),
+    url(r'^member_delete/', views.member_delete),
+    url(r'^member_edit/', views.member_edit),
+    url(r'^billing/', views.billing),
+    url(r'^contact/', views.contact),
+    url(r'^ice_management/', views.ice_management),
+    url(r'^impressum/', views.impressum),
+    url(r'^login/', views.login),
+    url(r'^material/', views.material),
+    url(r'^trainings/', views.trainings),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,50 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def add_member(request):
-    return render(request, 'add_member.html')
 
-def directory(request):
-    return render(request, 'directory.html')
+def member_add(request):
+    return render(request, 'member_add.html')
+
+
+def member_directory(request):
+    return render(request, 'member_directory.html')
+
+
+def member_delete(request):
+    return render(request, 'member_delete.html')
+
+
+def member_edit(request):
+    return render(request, 'member_edit.html')
+
+
+def billing(request):
+    return render(request, 'billing.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def ice_management(request):
+    return render(request, 'ice_management.html')
+
+
+def impressum(request):
+    return render(request, 'impressum.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def material(request):
+    return render(request, 'material.html')
+
+
+def trainings(request):
+    return render(request, 'trainings.html')
