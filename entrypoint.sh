bcffd43f2c23e63c09943bdcad94b23698886194
Gabor Tanz <gabor.tanz@gmail.com>
2017-11-10 10:28:11 +0100

    add wait for db in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Waiting for db to come up"
+while ! nc scm-db 3306; do
+  >&2 echo "db is unavailable - sleeping"
+  sleep 1
+done
+
 echo "Migrating db"
 python manage.py migrate
 echo "Starting server"
